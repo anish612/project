6c03e9d361ae255148c43a11f3a9ac8db915e0db
Anish Dhiman <82077105+anish612@users.noreply.github.com>
2022-08-16 19:27:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-# provider aws {
-#   profile =  "default"
-#   region = "ap-south-1"
-#   access_key = "AKIAZW3O6PUIA4DQT6Y2"
-#   secret_key = "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"
-  
-# }
-
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIAZW3O6PUIA4DQT6Y2"
-  secret_key = "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"
+provider aws {
+  profile =  "default"
+  region = "ap-south-1" 
 }
